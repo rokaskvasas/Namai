c5ed5873bfa730eae1e04866d9778eae5e8d1e91
rokaskvasas <rokas.kvasas@gmail.com>
2021-05-25 13:35:10 +0300

    Initial commit:

---
+++ src/main/java/House.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+
+public class House {
+    int id;
+    String title;
+    String address;
+    String zipCode;
+    String city;
+    double price;
+    boolean isSold;
+    TypeEnum type;
+
+    public House(int id, String title, String address, String zipCode, String city, double price, boolean isSold, TypeEnum type) {
+        this.id = id;
+        this.title = title;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.price = price;
+        this.isSold = isSold;
+        this.type = type;
+    }
+
+    static int QuantityOfHouses(ArrayList<House> array) {
+        return array.size();
+    }
+
+    static void printArray(ArrayList<House> array) {
+        for (House x : array) {
+            System.out.println(String.format("Id:%s, Title:%s, Address:%s, City:%s, Type:%s, Price:%s, Sold:%s", x.id, x.title, x.address, x.city, x.type, x.price, x.isSold));
+        }
+    }
+    static  void filterHouseType( ArrayList<House> array, ArrayList<House> house,ArrayList<House> tower, ArrayList<House> other,ArrayList<House> church) {
+        for (House x : array) {
+            switch (x.type) {
+                case HOUSE:
+                    house.add(x);
+                    break;
+                case TOWER:
+                    tower.add(x);
+                    break;
+                case OTHER:
+                    other.add(x);
+                    break;
+                case CHURCH:
+                    church.add(x);
+                    break;
+            }
+        }
+    }
+    static void filterHouseByPrice(ArrayList<House> array,ArrayList<House> normal,ArrayList<House> expensive){
+        for (House x : array) {
+            if (x.price < TaskHouse.expensivePrice) {
+                normal.add(x);
+            } else {
+                expensive.add(x);
+            }
+        }
+    }
+    static House theMostExpensive(ArrayList<House> array){
+        House mostexpensive=null;
+        for(House x:array) {
+            if(mostexpensive ==null){
+                mostexpensive=x;
+            }else{
+                if(x.price> mostexpensive.price){
+                    mostexpensive = x;
+                }
+            }
+        }
+        return mostexpensive;
+    }
+    static void findHouse(ArrayList<House> array, int id) {
+        for (House x : array) {
+            if (x.id==id) {
+                System.out.println(String.format("Id:%s, Title:%s, Address:%s, City:%s, Type:%s, Price:%s, Sold:%s", x.id, x.title, x.address, x.city, x.type, x.price, x.isSold));
+            }
+        }
+    }
+    static void sellhouse(int id,double price, ArrayList<House> array) {
+        for (House x : array) {
+            if (x.id == id) {
+                if (price > x.price) {
+                    x.price=price;
+                    x.isSold = true;
+                }
+            }
+        }
+    }
+    public String printInfo(){return String.format("Id:%s, Title:%s, Address:%s, City:%s, Type:%s, Price:%s, Sold:%s", this.id, this.title, this.address, this.city, this.type, this.price, this.isSold);}
+}
---
+++ src/main/java/TaskHouse.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class TaskHouse {
+    public static final double expensivePrice = 200000.00;
+
+    public static void main(String[] args) {
+        ArrayList<House> allHouses = new ArrayList<>();
+        allHouses.add(new House(1, "Mage Tower", "Eastern Kingdoms", "Elwynn forest", "Stormwind", 250420, false, TypeEnum.TOWER));
+        allHouses.add(new House(2, "Magni Bronzebeard hut", "Eastern Kingdoms", "Dun Morogh", "Ironforge", 50000, false, TypeEnum.HOUSE));
+        allHouses.add(new House(3, "Khadgar's Tower", "Outland", "Terrokar Forest", "Shattrath", 300000, false, TypeEnum.TOWER));
+        allHouses.add(new House(4, "Shadowfang Tower", "Northrend", "Grizzly Hills", "Bloodmoon isle", 75000, false, TypeEnum.TOWER));
+        allHouses.add(new House(5, "Mortwake's Tower", "Eastern Kingdoms", "Westfall", "Dagger Hills", 300050, false, TypeEnum.TOWER));
+        allHouses.add(new House(6, "Arcane sanctum", "Easter Kingdoms", "Eversong Woods", "Silvermoon", 90909, false, TypeEnum.TOWER));
+        allHouses.add(new House(7, "Thrall hut", "Kalimdor", "Durotar", "Orgrimmar", 200100, false, TypeEnum.HOUSE));
+        allHouses.add(new House(8, "Sylvanas hut", "Eastern Kingdoms", "Eversong Woods", "Silvermoon", 150500, false, TypeEnum.HOUSE));
+        allHouses.add(new House(9, "Vol'jin hut", "Kalimdor", "Durotar", "Orgrimmar", 60000, false, TypeEnum.HOUSE));
+        allHouses.add(new House(10, "Arthas hut", "Easter Kingdoms", "Elwynn forest", "Stormwind", 450000, false, TypeEnum.HOUSE));
+        allHouses.add(new House(11, "Light's Dawn Cathedral", "Lordaeron", "Silverpine Forest", "Gilneas", 190999, false, TypeEnum.CHURCH));
+        allHouses.add(new House(12, "Scarlet Monastery", "Eastern Kingdoms", "Tirisfal Glades", "Undercity", 602666, false, TypeEnum.CHURCH));
+        allHouses.add(new House(13, "Northshire Abbey", "Eastern Kingdoms", "Elwynn forest", "Stormwind", 120000, false, TypeEnum.CHURCH));
+        allHouses.add(new House(14, "The Violet citadel", "Northrend", "Crystalsong", "Dalaran", 515000, false, TypeEnum.CHURCH));
+        allHouses.add(new House(15, "Morgan's Plot", "Eastern Kingdoms", "Deadwing pass", "Karazhan", 40000, false, TypeEnum.CHURCH));
+        allHouses.add(new House(16, "The Stockade", "Eastern Kingdoms", "Elwynn forest", "Stormwind", 600700, false, TypeEnum.OTHER));
+        allHouses.add(new House(17, "Ragefire Chasm", "Kalimdor", "Durotar", "Orgrimmar", 35000, false, TypeEnum.OTHER));
+        allHouses.add(new House(18, "Siege of Orgrimmar", "Vale of Eternal Blossoms", "Shrine of Seven Stars", "Orgrimmar", 1300000, false, TypeEnum.OTHER));
+        allHouses.add(new House(19, "Illidari Training Grounds", "Outland", "Shadowmoon Valley", "Black temple", 180000, false, TypeEnum.OTHER));
+        allHouses.add(new House(20, "Gnomeregan", "Eastern Kingdoms", "Dun Morogh", "Ironforge", 245000, false, TypeEnum.OTHER));
+        ArrayList<House> expensiveHouses = new ArrayList<>();
+        ArrayList<House> normalHouses = new ArrayList<>();
+        ArrayList<House> Houses = new ArrayList<>();
+        ArrayList<House> Tower = new ArrayList<>();
+        ArrayList<House> Church = new ArrayList<>();
+        ArrayList<House> Other = new ArrayList<>();
+        House.filterHouseByPrice(allHouses,normalHouses,expensiveHouses);
+        House.filterHouseType(allHouses,Houses,Tower,Other,Church);
+        House.printArray(Tower);
+        System.out.println(House.QuantityOfHouses(allHouses));
+        House.findHouse(allHouses,9);
+        System.out.println(allHouses.get(0).printInfo());
+        System.out.println(House.theMostExpensive(allHouses).printInfo());
+        House.sellhouse(20,9652000,allHouses);
+        System.out.println(allHouses.get(19).printInfo());
+    }
+}
+
+
